3a1e9669f36ed339cefde0397971de5fb77e679b
quentin-metro <quentin.metro@gmail.com>
2023-05-11 15:19:45 +0200

    setup project

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+# from server import *
+
+"""
+@pytest.fixture
+def client():
+    app = create_app({"TESTING": True})
+    with app.test_client() as client:
+        yield client
+"""
